c918c881f190d9bde4688821194526ca4c583d38
SamuelEBG <samuel.ebgonzales@gmail.com>
2022-02-15 14:00:32 +0100

    git commit

--- src/main/java/pg42sg/task02b/MyBiDirectionalLinkedList.java
+++ src/main/java/pg42sg/task02b/MyBiDirectionalLinkedList.java
@@ -1,6 +1,46 @@
 package pg42sg.task02b;
 
-import org.pg4200.les02.list.MyLinkedList;
 
-public class MyBiDirectionalLinkedList extends MyLinkedList {
+import org.pg4200.les02.list.MyList;
+
+public class MyBiDirectionalLinkedList<T> implements MyList<T> {
+
+    private class ListNode{
+        T value;
+        ListNode next;
+    }
+    private ListNode head;
+    private ListNode tail;
+    private int size;
+
+
+    @Override
+    public void delete(int index) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return MyList.super.isEmpty();
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(T value) {
+        MyList.super.add(value);
+    }
+
+    @Override
+    public void add(int index, T value) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
