f5a10f65561758c71044c32d3f096bcdec74cc2f
SamuelEBG <samuel.ebgonzales@gmail.com>
2022-03-11 10:22:39 +0100

    från macjäveln

--- src/main/java/pg42sg/task04e/TriangleClassification.java
+++ src/main/java/pg42sg/task04e/TriangleClassification.java
@@ -1,16 +1,94 @@
 package pg42sg.task04e;
 
+// Import Classification.* to import
+import static pg42sg.task04e.TriangleClassification.Classification.*;
+
+
+public class TriangleClassification {
+    // Consider imports when creating test, enum has to be imported
+    // As static in the test class, so when we return a constant
+    // from the enum we just type the constant without the enum first.
+    public enum Classification {NOT_A_TRIANGLE, ISOSCELES, SCALENE, EQUILATERAL}
+
+    // This method also has to be imported as static, since static
+    // methods cannot be instantiated, they can be called from outside the class.
+    public static Classification classify(int a, int b, int c) {
+
+        // Any triangle cannot have negative or 0 length of size.
+        if(a <= 0 || b <= 0 || c <= 0){
+            return NOT_A_TRIANGLE;
+        }
+        // Triangle with equal sides.
+        if(a==b && b==c){
+            return EQUILATERAL;
+        }
+        // Here we check if one of the sides is too long, for example greater or
+        // equal to both the other sides.
+        // Returns the greatest value of parameters a, b
+        // and in this case it will also check against parameters b, c
+        int max = Math.max(a, Math.max(b, c));
+        // In the case of 1,2,3 => max (1, (3)) max=3 (c)
+        if ((max == a && max - b - c >= 0) ||
+                (max == b && max - a - c >= 0) ||
+                (max == c && max - a - b >= 0)) {   // check if max is greater or
+            return NOT_A_TRIANGLE;                  // equal to the other 2 numbers.
+        }
+
+        /*
+        if((a==b && c==a-1) || (a==c && b==a-1) || (b==c && a==b-1)){
+            return ISOSCELES;
+        }
+         */
+
+        // (a==b && c==a-1) || (a== c && b==a-1) || (b==c && a==b-1)
+
+        // Lastly we have equal long sides with 1 smaller side
+        // which will return false in the previous statement because the largest
+        // side minus the 2 other sides will return a negative result
+        // resulting in moving forward to this method.
+        if(a==b || a==c || b==c){
+            return ISOSCELES;
+        } else{
+            // none of the sides are equally long, but neither
+            // of the sides are longer than the other to create a
+            // false triangle.
+            return SCALENE;
+        }
+    }
+}
+
+/*
+
 public class TriangleClassification {
 
     public enum Classification {NOT_A_TRIANGLE, ISOSCELES, SCALENE, EQUILATERAL}
 
+
     public static Classification classify(int a, int b, int c) {
 
-        if(a < 0 || b < 0 || c < 0){
-            return Classification.NOT_A_TRIANGLE;
+        if(a <= 0 || b <= 0 || c <= 0){
+            return NOT_A_TRIANGLE;
         }
 
+        if(a==b && b==c){
+            return EQUILATERAL;
+        }
 
-        return null;
+        int max = Math.max(a, Math.max(b, c));
+
+        if ((max == a && max - b - c >= 0) ||
+                (max == b && max - a - c >= 0) ||
+                (max == c && max - a - b >= 0)) {
+            return NOT_A_TRIANGLE;
+        }
+
+        if(a==b || a==c || b==c){
+            return ISOSCELES;
+        } else {
+            return SCALENE;
+        }
     }
 }
+
+ */
+
--- src/test/java/pg42sg/task04e/TriangleClassificationTest.java
+++ src/test/java/pg42sg/task04e/TriangleClassificationTest.java
@@ -4,18 +4,142 @@ import org.junit.jupiter.api.Test;
 import pg42sg.task04e.TriangleClassification.Classification;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static pg42sg.task04e.TriangleClassification.Classification.NOT_A_TRIANGLE;
+import static pg42sg.task04e.TriangleClassification.Classification.*;
 import static pg42sg.task04e.TriangleClassification.classify;
 
 
 public class TriangleClassificationTest {
+    int max = Integer.MAX_VALUE;
 
     @Test
-    public void aNegativeTest(){
-
+    public void aNegativeTest() {
         Classification neg = classify(-1, 0, 0);
-
         assertEquals(NOT_A_TRIANGLE, neg);
     }
 
+    @Test
+    public void bNegativeTest(){
+        Classification neg = classify(0, -1, 0);
+        assertEquals(NOT_A_TRIANGLE, neg);
+    }
+
+    @Test
+    public void cNegativeTest(){
+        Classification neg = classify(0, 0, -1);
+        assertEquals(NOT_A_TRIANGLE, neg);
+    }
+
+    @Test
+    public void aZeroTest(){
+        Classification zero = classify(0, 2, 3);
+        assertEquals(NOT_A_TRIANGLE, zero);
+    }
+
+    @Test
+    public void bZeroTest(){
+        Classification zero = classify(3, 0, 3);
+        assertEquals(NOT_A_TRIANGLE, zero);
+    }
+
+    @Test
+    public void cZeroTest(){
+        Classification zero = classify(5, 5, 0);
+        assertEquals(NOT_A_TRIANGLE, zero);
+    }
+
+    @Test
+    public void allNegativesTest(){
+        Classification negatives = classify(-1,-1,-1);
+        assertEquals(NOT_A_TRIANGLE, negatives);
+    }
+
+    @Test
+    public void allZerosTest(){
+        Classification zeros = classify(0,0,0);
+        assertEquals(NOT_A_TRIANGLE, zeros);
+    }
+
+    @Test
+    public void allOnesTest(){
+        Classification allOnes = classify(1, 1, 1);
+        assertEquals(EQUILATERAL, allOnes);
+    }
+
+    @Test
+    public void allMaxTest(){
+        Classification allMax = classify(max,max,max);
+        assertEquals(EQUILATERAL, allMax);
+    }
+
+    @Test
+    public void aEqualsB(){
+        Classification aEqualsB = classify(2,2, 1);
+        assertEquals(ISOSCELES, aEqualsB);
+    }
+
+    @Test
+    public void aEqualsC(){
+        Classification aEqualsC = classify(2,1, 2);
+        assertEquals(ISOSCELES, aEqualsC);
+    }
+
+    @Test
+    public void bEqualsC(){
+        Classification bEqualsC = classify(1,2, 2);
+        assertEquals(ISOSCELES, bEqualsC);
+    }
+
+    @Test
+    public void aTooLarge(){
+        Classification a = classify(5,2,2);
+        assertEquals(NOT_A_TRIANGLE, a);
+    }
+
+    @Test
+    public void bTooLarge(){
+        Classification b = classify(2,5,2);
+        assertEquals(NOT_A_TRIANGLE, b);
+    }
+
+    @Test
+    public void cTooLarge(){
+        Classification c = classify(2,2,5);
+        assertEquals(NOT_A_TRIANGLE, c);
+    }
+
+    @Test
+    public void aMax(){
+        Classification aMax = classify(max-1, max, max);
+        assertEquals(ISOSCELES, aMax);
+    }
+
+    @Test
+    public void bMax(){
+        Classification bMax = classify(max, max-1, max);
+        assertEquals(ISOSCELES, bMax);
+    }
+
+    @Test
+    public void cMax(){
+        Classification cMax = classify(max, max, max-1);
+        assertEquals(ISOSCELES, cMax);
+    }
+
+    @Test
+    public void scalene(){
+        Classification sca = classify(2,3,4);
+        assertEquals(SCALENE, sca);
+    }
+
+    @Test
+    public void maxScalene(){
+        Classification maxSca = classify(max, max-1, max-2);
+        assertEquals(SCALENE, maxSca);
+    }
+
+    @Test
+    public void wanTooTri(){
+        Classification wanTooTri = classify(1,2,3);
+        assertEquals(NOT_A_TRIANGLE, wanTooTri);
+    }
 }
